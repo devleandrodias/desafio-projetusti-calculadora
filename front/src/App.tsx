7d8c99d801fb159719bc17b30db1c1a402648158
devleandrodias <leandro.dias_profissional@outlook.com.br>
2020-02-11 09:09:36 -0300

    Implementação da biblioteca de UI Antd

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import Calculator from "./components/Calculator";
 
 export default function App() {
-  return <Calculator value1={50} value2={45} />;
+  return <Calculator />;
 }
